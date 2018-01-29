dcecfc6961a649818e99a710e0e41130d8a23411
ankita dangi <ankita.dangi@zingbytes.com>
2018-01-29 15:15:27 +0530

    some new changes

--- config/initializers/paypal.rb
+++
@@ -1,6 +0,0 @@
-PayPal::Recurring.configure do |config|
-  config.sandbox = true
-  config.username = "satgurufuels-facilitator_api1.gmail.com"
-  config.password = "QBGK24YBCPD8D2ZH"
-  config.signature = "AOHboTRh7WyWByCcaTQ2YECu7C2WA4lmbhCFl4RMV8gJAJ9VyZ4YfWP1"
-end
